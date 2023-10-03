0125686e07aabeee11f0a143100911a6a787bcc5
Will Potter <williampotter701@gmail.com>
2023-10-02 21:19:57 -0700

    edited find and replace

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@
 //  - right side buttons: pokemon of the day, quiz, user journal entry
 
 // Global Variables 
+
 const catUrl = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random'; // Chuck Norris API
 const options = {
     method: 'GET',
@@ -15,31 +16,46 @@ const options = {
     }
 };
 
-//var apiKey =  '868927087amsh65a6ffffa5b7c46p19dcadjsn7079a2a63238';
+
+let apiKey = '868927087amsh65a6ffffa5b7c46p19dcadjsn7079a2a63238';
 
 
-// let quoteSection = document.querySelector('#quote');
-// let main = document.querySelector('main');
-
+let quoteSection = document.querySelector('#quote');
+let main = document.querySelector('main');
+let searchBtn = document.querySelector('#search-button')
 
 // Chuck Norris API Section
+searchBtn.addEventListener('click',
+    function replaceName(event) {
+        event.preventDefault();
 
-// fetch(catUrl, options)
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (data) {
-//     document.querySelector('#quote').innerHTML = data.value
-//     console.log(data);
+        fetch(catUrl, options)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
 
-//testing to see if we can replace chuck norris' name
-// let cnQuote = data.value;
-// console.log(cnQuote);
-// let pkmnQuote = cnQuote.replaceAll('Chuck Norris', 'Bulbasaur');
-// console.log(pkmnQuote);
+                console.log(data);
+                // testing to see if we can replace chuck norris' name
+                let pokeName = document.querySelector('#search-bar')
+                let cnQuote = data.value;
+                // let cnQuote = 'CHuck Norris is <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quidem blanditiis, perspiciatis minus aspernatur repellat. Nostrum aperiam accusamus sit blanditiis voluptatem aut magni dolorum. Odit voluptatum nobis sunt alias est.';
 
+                let pkmnQuote = cnQuote.replaceAll(/Chuck Norris/ig, pokeName.value.trim().charAt(0).toUpperCase() + (pokeName.value).slice(1));
+                console.log(pkmnQuote);
+                console.log(cnQuote);
+                let pluralNorris = /Chuck Norris'/;
+                if (cnQuote.includes(pluralNorris)) {
+                    cnQuote.replaceAll(/Chuck Norris'/ig, pokeName.value.trim().charAt(0).toUpperCase() + (pokeName.value).slice(1) + `'s`);
+                } else {
+                    document.querySelector('#norris-quote').textContent = pkmnQuote;
+                }
+            })
+    });
 //only problem is if chuck norris' name is used in possessive because of how apostrophe works.
 //need if statement to achieve this.
+
+
 
 // ===========================================
 
@@ -69,54 +85,54 @@ fetch(pokeUrl)
 
     })
 
-    function renderPkmnPage(data) {
-            //declared this for a more accessible scope so it can be used in function and when displaying content
-        let listOfGames = data.game_indices;
+function renderPkmnPage(data) {
+    //declared this for a more accessible scope so it can be used in function and when displaying content
+    let listOfGames = data.game_indices;
 
-        // this function logs every game the pokemon is in; dont know how to display all those names in setHTML; i'm used to doing textContent -Ryan
-        function gamesList() {
+    // this function logs every game the pokemon is in; dont know how to display all those names in setHTML; i'm used to doing textContent -Ryan
+    function gamesList() {
 
-            console.log(listOfGames);
+        console.log(listOfGames);
 
-            for (let i = 0; i < listOfGames.length; i++) {
-                console.log(listOfGames[i].version.name);
-            };
-        }
-
-        //DISPLAYING STAT CARD
-        // need for loop for stats; it would display as such >> '${data.stats[i].stat.name : ${data.stats[i].base_stat}'
-        // can replace abilities with stats or just add new category for stats.
-        //just noticed height and weight are in decimeters and hectograms. must multiply by 0.10 to get kg units, multiply by 2.205 to pounds
-        //multiply by 0.10 to get km, then multiply by 39.4 to get inches, then divide by 12 to get feet -- may need function for this conversion
-        //write formula to write height in feet, weight in pounds
-        // will need to write this in a for loop replacing index numbers with i.
-        //just noticed for typing: code is broken if we include both types and pokemon only has one. need if statement to include second type
-
-        let pkmnType = (data.types[0].type.name).charAt(0).toUpperCase() + (data.types[0].type.name).slice(1);
-        console.log(pkmnType);
-        let secondType = (data.types[1].type.name).charAt(0).toUpperCase() + (data.types[1].type.name).slice(1);
-
-        if (data.types[1].type.name) {
-
-            pkmnType = pkmnType.concat('/' + secondType)
-            console.log(pkmnType);
+        for (let i = 0; i < listOfGames.length; i++) {
+            console.log(listOfGames[i].version.name);
         };
-
-        let statCardHTML =
-            `<div>
-                <p class = "block"><strong>NAME:</strong> ${(data.name).charAt(0).toUpperCase() + (data.name).slice(1)}</p>
-                <p class = "block"><strong>TYPE:</strong>  ${pkmnType}</p>
-                <p class = "block"><strong>HEIGHT:</strong>  ${(((data.height * 0.1) * 39.4) / 12).toFixed(1)}'</p>
-                <p class = "block"><strong>WEIGHT:</strong>  ${((data.weight * 0.1) * 2.205).toFixed(1)} lbs </p>
-                <p class = "block"><strong>ABILITIES:</strong>  ${data.abilities[0].name} ${data.abilities[1].name}</p>
-                <p class = "block"><strong>MAIN GAMES FOUND IN:</strong>  ${listOfGames.map(game => game.version.name).join(', ')}</p> 
-    </div>`
-
-        // <p>MAIN GAMES FOUND IN: ${listOfGames.map(game => game.version.name).join(', ')}</p> 
-        //  this works and displays all the info. but i dont know what's happening. Not sure what .map is doing, where 'game' came from, and how arrow function works.
-
-        document.querySelector('#stat-page').setHTML(statCardHTML);
     }
+
+    //DISPLAYING STAT CARD
+    // need for loop for stats; it would display as such >> '${data.stats[i].stat.name : ${data.stats[i].base_stat}'
+    // can replace abilities with stats or just add new category for stats.
+    //just noticed height and weight are in decimeters and hectograms. must multiply by 0.10 to get kg units, multiply by 2.205 to pounds
+    //multiply by 0.10 to get km, then multiply by 39.4 to get inches, then divide by 12 to get feet -- may need function for this conversion
+    //write formula to write height in feet, weight in pounds
+    // will need to write this in a for loop replacing index numbers with i.
+    //just noticed for typing: code is broken if we include both types and pokemon only has one. need if statement to include second type
+
+    let pkmnType = (data.types[0].type.name).charAt(0).toUpperCase() + (data.types[0].type.name).slice(1);
+    console.log(pkmnType);
+    let secondType = (data.types[1].type.name).charAt(0).toUpperCase() + (data.types[1].type.name).slice(1);
+
+    if (data.types[1].type.name) {
+
+        pkmnType = pkmnType.concat('/' + secondType)
+        console.log(pkmnType);
+    };
+
+    let statCardHTML =
+        `<div>
+            <p class = "block"><strong>NAME:</strong> ${(data.name).charAt(0).toUpperCase() + (data.name).slice(1)}</p>
+            <p class = "block"><strong>TYPE:</strong>  ${pkmnType}</p>
+            <p class = "block"><strong>HEIGHT:</strong>  ${(((data.height * 0.1) * 39.4) / 12).toFixed(1)}'</p>
+            <p class = "block"><strong>WEIGHT:</strong>  ${((data.weight * 0.1) * 2.205).toFixed(1)} lbs </p>
+            <p class = "block"><strong>ABILITIES:</strong>  ${data.abilities[0].name} ${data.abilities[1].name}</p>
+            <p class = "block"><strong>MAIN GAMES FOUND IN:</strong>  ${listOfGames.map(game => game.version.name).join(', ')}</p> 
+        </div>`
+
+    // <p>MAIN GAMES FOUND IN: ${listOfGames.map(game => game.version.name).join(', ')}</p> 
+    //  this works and displays all the info. but i dont know what's happening. Not sure what .map is doing, where 'game' came from, and how arrow function works.
+
+    document.querySelector('#stat-page').setHTML(statCardHTML);
+}
 
 
 // function pokeStat(data) {
