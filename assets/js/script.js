86e427bda31e28f89bc59a2ab5f72fb2f4ab865c
mintyry <gitmintyry@gmail.com>
2023-10-06 00:03:22 -0700

    fixed disabled buttons again, margins for right side buttons not messing up again

--- assets/js/script.js
+++ assets/js/script.js
@@ -419,6 +419,7 @@ function renderSearchHistory() {
     for (let i = 0; i < pkmnArr.length && i < 3; i++) {
         let history = document.querySelector('#search-history')
         history.children[i].textContent = pkmnArr[i];
+        historySection.children[i].removeAttribute('disabled');
         // console.log(pkmnArr);
     }
 };
